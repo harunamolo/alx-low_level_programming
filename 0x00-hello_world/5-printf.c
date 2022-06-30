9fbba40d74b63bad7c7f214bd933077bed36fc48
harunamolo <amoloharun@gmail.com>
2022-06-30 05:12:45 -0700

    prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* Description: Main - prints out the string in standard input output.
+* Return: 0 if the string is a success.
+*/
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
