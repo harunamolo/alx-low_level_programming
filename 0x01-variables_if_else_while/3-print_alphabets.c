f979c63b7a0ab57391987fa9360cd50874c522c2
harunamolo <107302226+harunamolo@users.noreply.github.com>
2022-07-01 15:58:39 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-int lw = a;
-int up = A;
-while (lw <= z)
+int lw = 'a';
+int up = 'A';
+while (lw <= 'z')
 {
 putchar(lw);
 lw += 1;
 }
-while (up <= Z)
+while (up <= 'Z')
 {
 putchar(up);
 up += 1;
