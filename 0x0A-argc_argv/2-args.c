504420aaa0e0580072774e8d905fdaf9b7846cb0
harunamolo <107302226+harunamolo@users.noreply.github.com>
2022-07-23 10:39:44 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,25 @@
-three
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: argument count
+ * @argv: argunment vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
