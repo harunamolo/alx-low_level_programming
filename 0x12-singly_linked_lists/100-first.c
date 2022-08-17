bb03eb14fe3ce8099290d96a3fd3252c91b0104e
harunamolo <107302226+harunamolo@users.noreply.github.com>
2022-08-17 20:46:58 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,14 @@
-six
+#include <stdio.h>
+
+void print_something(void) __attribute__ ((constructor));
+
+/**
+ *print_something - prints a msg before the main
+ */
+
+void print_something(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
+
